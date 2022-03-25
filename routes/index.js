85f6d31a2b56a4e176624ae7d9c9d3192392fb29
MirMM1472 <meermazhar@hztech.biz>
2022-03-25 19:20:13 +0500

    update added

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,25 @@ router.get("/Save", async function (req, res, next) {
   // res.render("index", { title: "Express" });
 });
 
+router.get("/getTrain", async function (req, res, next) {
+  console.log(req.query);
+  const train = await db.trains.find({ _id: req.query.id });
+  res.status(200).json(train[0]);
+  // res.render('index', { title: 'Express', trains });
+});
+
+router.get("/edit", async function (req, res, next) {
+  console.log(req.query);
+  // const obj = req.query;
+  // delete obj._id;
+  // console.log(obj);
+  const results = await db.trains.updateOne(
+    { _id: req.query._id },
+    { $set: req.query }
+  );
+  console.log(results);
+  res.status(200).json(results);
+  // res.render("index", { title: "Express" });
+});
+
 module.exports = router;
